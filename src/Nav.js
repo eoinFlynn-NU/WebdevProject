109842c76546f1560093035fa2b5267ee3c44a57
Eoin Flynn <flynn.j@northeastern.edu>
2023-11-29 13:23:50 -0500

    nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,10 @@
-
+import "./Nav.css"
 
 import { Link, useLocation } from "react-router-dom";
 function Nav() {
   const { pathname } = useLocation();
   return (
-    <nav className="nav nav-tabs mt-2 container">
+    <nav className="nav mt-2 container">
       <Link className={`nav-link ${pathname.includes("HOME") ? "active" : ""}`} to="/HOME">Home</Link>
       <Link className={`nav-link ${pathname.includes("SEARCH") ? "active" : ""}`} to="/SEARCH">Search</Link>
       <Link className={`nav-link ${pathname.includes("PROFILE") ? "active" : ""}`} to="/PROFILE">Profile</Link>
--- src/home/index.js
+++ src/home/index.js
@@ -1,12 +1,14 @@
 import Nav from "../Nav";
 import AnonymousHome from "./AnonymousHome";
+import "./home.css"
+import "bootstrap"
 
 
 
 
 function Home() {
     return (
-        <div>
+        <div className="container">
             <Nav/>
             <AnonymousHome/>
         </div>
