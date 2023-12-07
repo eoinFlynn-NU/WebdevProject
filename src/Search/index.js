f19acdba95e173e4229529e96f5fdf7916065ade
Eoin Flynn <flynn.j@northeastern.edu>
2023-12-06 19:22:34 -0500

    search start

--- src/Search/index.js
+++ src/Search/index.js
@@ -1,7 +1,22 @@
+import "./search.css"
+
 function Search(){
     return(
-        <div>
-            <h1>search</h1>
+        <div className="home-background">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Your Website</a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <form class="form-inline ml-auto">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+                </nav>
         </div>
     )
 }
